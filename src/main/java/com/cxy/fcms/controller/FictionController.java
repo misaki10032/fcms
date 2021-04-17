d7f768da44ef1b64dc403964bf286b81ef5173a4
blank <1069664381@qq.com>
2021-04-17 16:12:14 +0800

    2021-4-17 15:53 CXY ---fiction数据接口定义完成, 新增layUI工具类 , 转化json数据

--- src/main/java/com/cxy/fcms/controller/FictionController.java
+++ src/main/java/com/cxy/fcms/controller/FictionController.java
@@ -7,19 +7,26 @@ import com.cxy.fcms.util.LayuiReplay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class FictionController {
     @Autowired
     FictionService fictionService;
     @GetMapping("/getfictions")
+    @ResponseBody
     public Object  getAllFictions() throws JSONException {
         List<ComFiction> fiction = fictionService.getFiction();
         return new LayuiReplay<ComFiction>(0, "OK", fiction.size(), fiction);
+    }
+    @GetMapping("/addfictionWindow")
+    public String toAddFictionWindow(){
+        return "back/fiction/addFiction";
     }
 
 }
